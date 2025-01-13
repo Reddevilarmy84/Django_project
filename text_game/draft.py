202ea0a885febc39e1c0a0f6dabb23454856e5e4
Reddevilarmy84 <Reddevil.army84@gmail.com>
2025-01-13 11:42:45 +0300

    style playlist

--- text_game/draft.py
+++ text_game/draft.py
@@ -1,7 +1,9 @@
-for x in range(0, 11):
-    print((lambda x: x**2)(x), end=" ")
+# 
+#
+y = 2
+for x in range(0, 11): print(f"{x} в степени {y} равно {(lambda x, y: x**y)(x, y)}")
 
 x = 5
 y = 5
 a = lambda x, y: x*y
-#print(a(x, y))
+print (f"Печатаем результат лямбда функции {a(x, y)} ")
