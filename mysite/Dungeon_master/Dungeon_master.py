dd94bfbc6db3cf6cc879c5f6059ec37b9f3be006
Reddevilarmy84 <reddevil.army84@gmail.com>
2025-02-27 23:28:14 +0300

    реализовать исследование 1 пещеры

--- mysite/Dungeon_master/Dungeon_master.py
+++ mysite/Dungeon_master/Dungeon_master.py
@@ -1,35 +1,10 @@
 import os
 import json
+import math
 
 path_to_json_DM = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Dungeon_master.json')
 
-class Colors:
-    black = '\033[30m'
-    red = '\033[31m'
-    green = '\033[32m'
-    yellow = '\033[33m'
-    blue = '\033[34m'
-    purple = '\033[35m'
-    light_blue = '\033[36m'
-    grey = '\033[37m'
 
-    black_back = '\033[40m'
-    red_back = '\033[41m'
-    green_back = '\033[42m'
-    yellow_back = '\033[43m'
-    blue_back = '\033[44m'
-    purple_back = '\033[45m'
-    light_blue_back = '\033[46m'
-    grey_back = '\033[47m'
-
-    reset = '\033[0m'  #сброс
-    bolt = '\033[1m'  #жирный
-    light = '\033[2m'  #Блёклый
-    curs = '\033[3m'  #Курсив
-    line = '\033[4m'  #Подчёркнутый
-    blink = '\033[5m'  # Редкое мигание
-    blink_fast = '\033[6m'  # Частое мигание
-    inv = '\033[7m'  # Смена цвета фона с цветом текста
 
 
 class Mob:
@@ -47,13 +22,13 @@ class Mob:
         self.hp = lvl*50
         self.mp = lvl*30
 
-class Mage:
+class Hero:
     exp = 0
     exp_to_lvl = 70
     lvl=1
     type= 'хз кто'
-    hp = 300
-    hp_max = 300
+    hp = 100
+    hp_max = 200
     mp = 100
     mp_max = 100
     crit = 30
@@ -102,6 +77,38 @@ class Mage:
         self.spec3_lvl = math.floor(self.lvl/10)
         self.spec3_pwr = math.floor(self.lvl/10) * 147
 
+
+class Location:
+    length = 0
+    path_traveled = 0
+
+class Colors:
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    blue = '\033[34m'
+    purple = '\033[35m'
+    light_blue = '\033[36m'
+    grey = '\033[37m'
+
+    black_back = '\033[40m'
+    red_back = '\033[41m'
+    green_back = '\033[42m'
+    yellow_back = '\033[43m'
+    blue_back = '\033[44m'
+    purple_back = '\033[45m'
+    light_blue_back = '\033[46m'
+    grey_back = '\033[47m'
+
+    reset = '\033[0m'  #сброс
+    bolt = '\033[1m'  #жирный
+    light = '\033[2m'  #Блёклый
+    curs = '\033[3m'  #Курсив
+    line = '\033[4m'  #Подчёркнутый
+    blink = '\033[5m'  # Редкое мигание
+    blink_fast = '\033[6m'  # Частое мигание
+    inv = '\033[7m'  # Смена цвета фона с цветом текста
 
 def dict_to_json(dict_list, filename):
     try:
--- mysite/Dungeon_master/views.py
+++ mysite/Dungeon_master/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .Dungeon_master import dict_to_json, json_to_dict, path_to_json_DM, Colors, Mage, Mob
-player = Mage()
-mob = Mob()
+from .Dungeon_master import dict_to_json, json_to_dict, path_to_json_DM, Colors, Hero, Mob, Location
 
+player = Hero()
+mob = Mob()
+loc = Location()
 
 def Dungeon_master(request, action: str = None):
     json = json_to_dict(path_to_json_DM)
@@ -33,6 +34,36 @@ def Dungeon_master(request, action: str = None):
             for i in json:
                 if i["id"] == "config":
                     i["phase"] = "choose_location"
+    # отслеживание фазы выбор локации
+    if phase == "choose_location":
+        if action == "cave_1":
+            loc.length = 10
+            loc.path_traveled = 0
+            for i in json:
+                if i["id"] == "config":
+                    i["phase"] = "exploration"
+    # отслеживание фазы локации
+    if phase == "exploration":
+        print(f"Пройдено: {loc.path_traveled}")
+        if action == "do_step":
+            if loc.path_traveled == loc.length-1:
+                loc.path_traveled += 1
+                for i in json:
+                    if i["id"] == "config":
+                        i["phase"] = "exploration_completed"
+                        print("Локация пройдена")
+            else:
+                loc.path_traveled += 1
+    # отслеживание фазы локация пройдена
+    if phase == "exploration_completed":
+        if action == "complete":
+            for i in json:
+                if i["id"] == "config":
+                    i["phase"] = "choose_location"
+            loc.path_traveled = 0
+            player.exp += 100
+            if player.exp >= player.exp_to_lvl:
+                player.up_lvl(1)
 
 
     print(json)
@@ -43,5 +74,6 @@ def Dungeon_master(request, action: str = None):
         'json': json,
         'player': player,
         'mob': mob,
+        'loc': loc,
     }
     return render(request, 'Dungeon_master/main.html', context)
